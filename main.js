f21574e5cea9e210d3c33e1c4f36a6a4e6ba84df
JaehyunCoding <kq1945@gmail.com>
2023-02-23 10:55:47 -0500

    Slow but keep going on

--- main.js
+++ main.js
@@ -2,7 +2,47 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+function isAdmin (user){
+    if (user.userRole === "ADMIN"){
+        return true
+    }   else {
+        return false
+    }
+}
 
+function getEmail (user){
+    let firstName = user.firstName.toLowerCase()
+    let lastName = user.lastName.toUpperCase()
+    return firstName + "." + lastName + "@codeimmersives.com"
+}
+
+function getPlaylistLength (playlist) {
+    return playlist.songs.length
+}
+
+function getHardestHomework (homeworkArray) {
+    let x = ""
+    if(homework.length === 0){
+        return x
+    }   
+    let lowestHw = homeworkArray[0]
+
+    for(let i = 0; i < homeworkArray.length; i++){
+        if(homeworkArray[i].averageScore < lowestHw.averageScore){
+            lowestHw = homeworkArray[i];
+        }
+    }
+
+    return lowestHw.name;
+}
+
+function createPhonebook (names, numbers){
+    let phonebook = {}
+    for (let i = 0; i < names.length; i++){
+        phonebook[names[i]] = numbers[i]
+    }
+    return phonebook
+}
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
